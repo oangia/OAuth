0cdb3ff99d3abe8496d5af9a6ededd5934e75ede
oangia <hqnhatdn@gmail.com>
2016-07-25 15:14:43 +0700

    Minor bug

--- src/Facebook/Facebook.php
+++ src/Facebook/Facebook.php
@@ -2,11 +2,11 @@
 
 namespace oangia\OAuth;
 
-use oangia\Curl\Curl;
+use oangia\CUrl\CUrl;
 
 class Facebook {
 	public function getGraph( $fields, $access_token ) {
-		$url = 'https://graph.facebook.com/me?fields=' . $fields . '&access_token' . $access_token;
+		$url = 'https://graph.facebook.com/me?fields=' . $fields . '&access_token=' . $access_token;
 
 		return  json_decode( Curl::curl_get_file_contents( $url ) );
 	}
