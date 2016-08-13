e4eb18356fb6fd6582d16b989719a9b322d88d67
oangia <hqnhatdn@gmail.com>
2016-08-13 10:55:38 +0700

    Fix get token feature

--- src/GetTwitterTokenTrait.php
+++ src/GetTwitterTokenTrait.php
@@ -9,26 +9,23 @@ trait GetTwitterTokenTrait {
     {
         if(isset($_REQUEST['oauth_token'])) {
             dd([
-                'oauth_token'         => \Session::get('token'),
-                'oauth_token_secret'  => \Session::get('token_secret'),
                 'oauth_verifier'      =>$_REQUEST['oauth_verifier']
             ]);
         } else {
             $connection = new Twitter(env('TWITTER_API'), env('TWITTER_SECRET'));
             $request_token = $connection->getRequestToken($request->url());
-
-            \Session::put('token',$request_token['oauth_token']);
-            \Session::put('token_secret', $request_token['oauth_token_secret']);
-            \Session::save();
-
             if($connection->http_code == '200')
             {
                 $twitter_url = $connection->getAuthorizeURL($request_token['oauth_token']);
-                header('Location: ' . $twitter_url); 
+                dd([
+                    'oauth_token'         => $request_token['oauth_token'],
+                    'oauth_token_secret'  => $request_token['oauth_token_secret'],
+                    'url'                 => '<a href="' . $twitter_url . '">Link</a>';
+                ]); 
                 exit;
             }else{
                 die("error connecting to twitter! try again later!");
-            }
+            }      
         }
     }
 }
