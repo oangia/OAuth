0c2a9af160f0c1c86153ad57e33c5029e5fb80e6
oangia <hqnhatdn@gmail.com>
2016-11-24 14:03:26 +0700

    Fix google login

--- src/Google.php
+++ src/Google.php
@@ -5,10 +5,10 @@ namespace oangia\OAuth;
 use oangia\CUrl\CUrl;
 
 class Google {
-	// https://accounts.google.com/o/oauth2/auth?response_type=code&client_id=1043035954080-fgu7mekck12eetsfce9t625e5hm6md59.apps.googleusercontent.com&redirect_uri=http://localhost/google&scope=email
-
 	public function getInfo( $access_token ) {
 		$url = 'https://www.googleapis.com/plus/v1/people/me?access_token=' . $access_token;
+
+		$curl = new CUrl;
 
 		return  json_decode( $curl->connect( 'GET', $url ) );
 	}
