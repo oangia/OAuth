458725e3641027e3705a4eea7e95e64ca358c2c9
oangia <hqnhatdn@gmail.com>
2018-09-19 10:27:48 +0700

    New url

--- src/Google.php
+++ src/Google.php
@@ -7,7 +7,8 @@ use oangia\OAuth\Exceptions\TokenInvalidException;
 
 class Google {
     public function getInfo( $access_token ) {
-        $url = 'https://www.googleapis.com/plus/v1/people/me?access_token=' . $access_token;
+        # v1 https://www.googleapis.com/plus/v1/people/me?access_token=
+        $url = 'https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=' . $access_token;
         $curl = new CUrl;
         $info = json_decode($curl->connect('GET', $url), true);
         return $this->parse($info);
